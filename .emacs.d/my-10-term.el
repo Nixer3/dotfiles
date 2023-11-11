feefc26d43b04936a60585a20e5afe7e85e3c5ca
Matej Skulský <matej.skulsky@gmail.com>
2023-11-11 14:38:27 +0100

    emacs: fonts VTERM + shorten y/n

--- .emacs.d/my-10-term.el
+++ .emacs.d/my-10-term.el
@@ -1,24 +1,29 @@
-(use-package vterm
-  :preface
-  (defun vterm-bottom ()
+(defun vterm-bottom ()
     "Split window and spawn vterm there"
     (interactive)
     (let* ((ignore-window-parameters t)
            (dedicated-p (window-dedicated-p)))
-      (split-window-vertically)
-      (other-window 1)
-      (vterm)))
+        (split-window-vertically)
+        (other-window 1)
+        (vterm))
+)
+(defun my/setup-vterm-custom-font ()
+  "Set custom font for vterm."
+  (setq buffer-face-mode-face '(:family "MesloLGSDZ Nerd Font Mono" :height 120))
+  (buffer-face-mode t)
+)
 
+(use-package vterm
   :ensure t
-  :hook (vterm-mode . (lambda ()
-                        (setq buffer-face-mode-face '(:family "FiraCode-Regular" :height 200))
-                        (buffer-face-mode t)))
+  :hook (vterm-mode . my/setup-vterm-custom-font)
 
   :commands vterm
   :config
   (setq vterm-always-compile-module t)
   (setq vterm-max-scrollback 10000)
   (setq vterm-shell "/usr/bin/zsh")
+
+ 
 
   :bind
   ("<M-f12>" . vterm-bottom))
